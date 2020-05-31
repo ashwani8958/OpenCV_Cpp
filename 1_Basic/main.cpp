d373bc75e293eccb462a4ea56b263fe5ab51d26e
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-05-31 09:40:29 +0530

    commit

---
+++ 1_Basic/main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  1_Basic
+//
+//  Created by Ashwani on 31/05/20.
+//  Copyright © 2020 Ashwani. All rights reserved.
+//
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+//opencv Library
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+
+int main(int argc, const char** argv)
+{
+  
+    //Open the image
+    Mat color = imread("../images/lena_color.bmp");
+
+    if(!color.data)
+    {
+        cout << "Enable to open the image! Check the Path or check image is available for operation" << endl;
+        return -1;
+    }
+
+    //Convert the image to gray scale
+    Mat gray;
+//    cvtColor(color, gray, COLOR_BGR2GRAY);
+    gray = imread("../images/lena_color.bmp", IMREAD_GRAYSCALE);
+
+    //save grayscale image version
+    imwrite("../images/lena_gray.bmp", gray);
+
+    //Print value of some function with opencv function
+    int myRow = color.cols - 1;
+    int myCol = color.rows - 1;
+    Vec3b pixel = color.at<Vec3b>(myRow, myCol);
+    cout << "Pixel value (B,G,R): (" << (int)pixel[0] << "," << (int)pixel[1] << "," << (int)pixel[2] << ")" << endl;
+
+
+    //namedWindow("Gray Image", WINDOW_AUTOSIZE);
+    imshow("Lena Color", color);
+    imshow("Lena Gray", gray);
+    waitKey(0);
+
+    //release the memory
+    color.release();
+    gray.release();
+
+    return 0;
+}
